3daad3f4edf8d72ee0286ac4d6a8c2d5e9c10308
skyclouds2001 <skyclouds2001@163.com>
2023-07-08 00:42:26 +0800

    feat: remove source files

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,4 @@
 import { createApp } from 'vue'
-import '@/style.css'
 import App from '@/App.vue'
-import router from '@/router'
-import store from '@/store'
 
-createApp(App).use(router).use(store).mount('#app')
+createApp(App).mount('#app')
