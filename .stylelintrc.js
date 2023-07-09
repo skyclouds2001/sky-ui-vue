3b7698a31287ae8e70a8a3b9264b332f45b7041a
skyclouds2001 <skyclouds2001@163.com>
2023-07-09 14:11:52 +0800

    build: update stylelint config

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -2,12 +2,6 @@ module.exports = {
   extends: ['stylelint-config-standard', 'stylelint-config-recess-order'],
   plugins: [],
   rules: {
-    'at-rule-no-unknown': [
-      true,
-      {
-        ignoreAtRules: ['tailwind', 'layer', 'apply', 'variants', 'responsive', 'screen'],
-      },
-    ],
     'no-empty-source': null,
   },
   overrides: [
