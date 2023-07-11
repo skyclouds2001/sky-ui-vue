b987f27b439f70644b869892a33f9b7cfee5f549
skyclouds2001 <95597335+skyclouds2001@users.noreply.github.com>
2023-07-11 14:41:19 +0800

    fix: add default export

--- src/index.ts
+++ src/index.ts
@@ -6,4 +6,6 @@ export { default as SkyDivider } from './Divider'
 
 export { default as SkyLink } from './Link'
 
+export { default as SkySpace } from './Space'
+
 export { default as SkyText } from './Text'
